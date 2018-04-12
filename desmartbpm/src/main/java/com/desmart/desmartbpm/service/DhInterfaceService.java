a33b06df2dcd5725f4ae5cef7a0f37edd93b7a36
zhaowei <zhaowei@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-04-12 09:45:49 +0000

    接口管理Service层

---
+++ desmartbpm/src/main/java/com/desmart/desmartbpm/service/DhInterfaceService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.desmart.desmartbpm.service;
+
+import java.util.List;
+
+import com.desmart.desmartbpm.entity.DhInterface;
+
+/**  
+* <p>Title: DhInterfaceService</p>  
+* <p>Description: 接口管理Service接口层</p>  
+* @author zhaowei  
+* @date 2018年4月12日  
+*/
+public interface DhInterfaceService {
+	
+	List<DhInterface> listDhInterface();
+}
