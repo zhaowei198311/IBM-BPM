52a5a1150bb16dfa95a88a8357c15d15ad4aeeab
zhaowei <zhaowei@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-04-12 09:27:27 +0000

    接口管理dao

---
+++ desmartbpm/src/main/java/com/desmart/desmartbpm/dao/DhInterfaceDao.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.desmart.desmartbpm.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.desmart.desmartbpm.entity.DhInterface;
+
+/**  
+* <p>Title: DhInterfaceDao</p>  
+* <p>Description: 接口管理数据访问层</p>  
+* @author zhaowei  
+* @date 2018年4月12日  
+*/
+@Repository
+public interface DhInterfaceDao {
+	
+	/**
+	 * 添加新的接口
+	 * @param dhInterface 接口实体类
+	 * @return
+	 */
+	int save(DhInterface dhInterface);
+	
+	/**
+	 * 删除接口
+	 * @param Interfaceid 接口id
+	 * @return
+	 */
+	int delete(int Interfaceid);
+	
+	/**
+	 * 查询所有接口
+	 * @return list返回界面
+	 */
+	List <DhInterface> listAll();
+	
+	
+	
+}
