50bd1c6dbd14d199a51b258075ad759cb809330c
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-01 08:43:44 +0000

    修改shiro realm

--- bpm/src/main/java/com/desmart/desmartportal/shiro/MyRealm.java
+++ bpm/src/main/java/com/desmart/desmartportal/shiro/MyRealm.java
@@ -3,17 +3,26 @@
  */
 package com.desmart.desmartportal.shiro;
 
+import java.util.List;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.desmart.desmartportal.common.Const;
+import com.desmart.desmartportal.service.UserProcessService;
+import com.desmart.desmartsystem.entity.SysUser;
+import com.desmart.desmartsystem.service.SysUserService;
 
 /**  
 * <p>Title: MyRealm</p>  
@@ -23,6 +32,9 @@ import org.apache.shiro.subject.PrincipalCollection;
 */
 public class MyRealm extends AuthorizingRealm {
 
+	@Autowired
+	private SysUserService sysUserService;
+	
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
 		 SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
@@ -32,23 +44,27 @@ public class MyRealm extends AuthorizingRealm {
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		
+		System.out.println("***********shiro login  start**************");
 		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken)token;
         String username = usernamePasswordToken.getUsername();
         String password = new String((char[])token.getCredentials());
 
-        System.out.println("======= MyRealm.doGetAuthenticationInfo() start =========");
         System.out.println("username: " + username + ", password: " + password);
-
-        if (!"liubei".equals(username) && !"caocao".equals(username)) {
+        //login
+        List<SysUser> userList = sysUserService.login(username, password);
+        if (userList.size() == 0) { 
+        	 throw new UnknownAccountException();//没找到帐号  
+        }  
+        /*if (!"liubei".equals(username) && !"caocao".equals(username)) {
             throw new AuthenticationException("用户不存在");
-        }
-
+        }*/
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(username, password.toCharArray(), this.getName());
         Session session = SecurityUtils.getSubject().getSession();
         session.setAttribute("_currUserNum", username);
         session.setAttribute("_password", password);
-
-        System.out.println("======= MyRealm.doGetAuthenticationInfo() finish =======");
+        session.setAttribute(Const.CURRENT_USER, userList.get(0).getUserId());
+        System.out.println("***********shiro login  end**************");
         return authenticationInfo;
 	}
 
