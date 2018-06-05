3222411b660e6c8fd3c1a76de7c6ba22ecadb465
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-05 07:50:03 +0000

    修改bpm模块中shiro realm

--- bpm/src/main/java/com/desmart/desmartbpm/shiro/MyRealm.java
+++ bpm/src/main/java/com/desmart/desmartbpm/shiro/MyRealm.java
@@ -1,5 +1,8 @@
 package com.desmart.desmartbpm.shiro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
@@ -7,34 +10,77 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.desmart.common.util.DataTool;
+import com.desmart.desmartbpm.controller.DhProcessMetaController;
+import com.desmart.desmartportal.common.Const;
+import com.desmart.desmartsystem.entity.SysResource;
+import com.desmart.desmartsystem.entity.SysUser;
+import com.desmart.desmartsystem.service.SysResourceService;
+import com.desmart.desmartsystem.service.SysUserService;
 
 public class MyRealm extends AuthorizingRealm {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(MyRealm.class);
+	
+	@Autowired
+	private SysUserService sysUserService;
+	
+	@Autowired
+	private SysResourceService sysResourceService;
+	
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        
+    	log.info("进入doGetAuthorizationInfo获取用户权限...");
+    	String userId = SecurityUtils.getSubject().getSession().getAttribute(Const.CURRENT_USER).toString();
+    	List<SysResource> resouceList = null;
+    	try {
+    		//通过用户ID查询当前用户的权限list
+			resouceList = sysResourceService.selectResourceByUserId(userId);
+			System.out.println(resouceList.get(0).getResourceCode().toString());
+		} catch (Exception e) {
+			log.info("查询用户权限出错...");
+			e.printStackTrace();
+		}
+    	
+    	SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+    	//新建一个list,把上面查询得到的权限码放进SimpleAuthorizationInfo中
+    	List<String> resource = new ArrayList<String>();
+		for (SysResource sysResource : resouceList) {
+			//将数据库中的权限标签放入集合
+			resource.add(sysResource.getResourceCode());
+		}
+		info.addStringPermissions(resource);
         return info;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken)token;
+    	
+    	System.out.println("***********shiro login  start**************");
+		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken)token;
         String username = usernamePasswordToken.getUsername();
         String password = new String((char[])token.getCredentials());
 
-        System.out.println("======= MyRealm.doGetAuthenticationInfo() start =========");
         System.out.println("username: " + username + ", password: " + password);
-
-        if (!"liubei".equals(username) && !"caocao".equals(username)) {
+        //login
+        List<SysUser> userList = sysUserService.login(username, DataTool.encodeMD5(password));
+        if (userList.size() == 0) { 
+        	 throw new UnknownAccountException();//没找到帐号  
+        }  
+        /*if (!"liubei".equals(username) && !"caocao".equals(username)) {
             throw new AuthenticationException("用户不存在");
-        }
-
+        }*/
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(username, password.toCharArray(), this.getName());
         Session session = SecurityUtils.getSubject().getSession();
         session.setAttribute("_currUserNum", username);
         session.setAttribute("_password", password);
-
-        System.out.println("======= MyRealm.doGetAuthenticationInfo() finish =======");
+        session.setAttribute(Const.CURRENT_USER, userList.get(0).getUserId());
+        System.out.println("***********shiro login  end**************");
         return authenticationInfo;
     }
 
