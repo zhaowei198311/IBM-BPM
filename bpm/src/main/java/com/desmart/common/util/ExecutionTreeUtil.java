8a42883201445b7f4b1896e3db11b628dde9dd40
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-07 03:21:29 +0000

    修改工具方法

--- bpm/src/main/java/com/desmart/common/util/ExecutionTreeUtil.java
+++ bpm/src/main/java/com/desmart/common/util/ExecutionTreeUtil.java
@@ -25,55 +25,72 @@ public class ExecutionTreeUtil {
      * @param jsonObject 由流程详细信息转化来的JSONObject
      * @return
      */
-	public static final Map<Object, Object> queryTokenId(int taskId, JSONObject jsonObject){
+	public static final Map<Object, Object> queryTokenId(int taskId, JSONObject json){
     	// 解析jsonObject
-    	JSONObject data = jsonObject.getJSONObject("data").getJSONObject("executionTree").getJSONObject("root");
-    	JSONArray childrenList = data.getJSONArray("children");
-    	HashMap<Object, Object> map = util(childrenList, taskId);
-    	System.out.println("map:" + map);
-    	return map;
+    	JSONObject jsonObject = json.getJSONObject("data").getJSONObject("executionTree").getJSONObject("root");
+		JSONArray jsonArray = jsonObject.getJSONArray("children");
+		//第一次遍历children数组对象
+		HashMap<Object,Object> hashMap = new HashMap<>();
+		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
+			JSONObject object = (JSONObject) iterator.next();
+			if(null != object.get("createdTaskIDs")) {
+				//此处比较createdTaskIDs数组中是否包含taskId
+				JSONArray taskIdsArray = object.getJSONArray("createdTaskIDs");
+				for (int i = 0; i < taskIdsArray.size(); i++) {
+					if (String.valueOf(taskId).equals(taskIdsArray.get(i))) {
+						hashMap.put("tokenId", object.get("tokenId"));
+				        hashMap.put("preTokenId", null);
+				        return hashMap;
+					}
+				}
+			}else{
+				//每次循环下级时候把此级tokenId放入list中
+				preTokenIdList.add(object.get("tokenId"));
+				JSONArray jsonArray2 = object.getJSONArray("children");
+				HashMap utilMap = util(jsonArray2, taskId);
+				if (utilMap != null) {
+					/*System.out.println(utilMap.get("tokenId"));
+					System.out.println(utilMap.get("preTokenId"));*/
+					return utilMap;
+				} else {
+					System.out.println("未查到结果...");
+				}
+			}
+		}
+		return null;
     }
 	
 	public static HashMap<Object, Object> util(JSONArray jsonArray, int taskId) {
 		
-		HashMap<Object, Object> hashMap = new HashMap<>();
-		JSONObject result = new JSONObject();
-		for (int i = 0; i < jsonArray.size(); i++) {
-			// for (Iterator var = jsonArray.iterator(); var.hasNext();) {
-			result = (JSONObject) jsonArray.get(i);
-			preTokenIdList.add(result.get("tokenId"));
-			if (null != result.get("createdTaskIDs")) {
-				// 获取createdTaskIDs，此处createdTaskIDs是数组
-				JSONArray jsonArray2 = result.getJSONArray("createdTaskIDs");
-				// 迭代createdTaskIDs，找到能匹配taskId参数的
-				for (Iterator iterator = jsonArray2.iterator(); iterator.hasNext();) {
-					String object = (String) iterator.next();
-					if (String.valueOf(taskId).equals(object)) {
-						hashMap.put("createdTaskIDs", result.get("createdTaskIDs"));
-						hashMap.put("tokenId", result.get("tokenId"));
-						hashMap.put("preTokenId", null);
-						if (preTokenIdList.size() > 0) {
-							Collections.reverse(preTokenIdList);
-							for (int j = flag; j > 0; j--) {
-								if (preTokenIdList.get(flag) != null) {
-									hashMap.put("preTokenId", preTokenIdList.get(j));
-									break;
-								}
+		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
+			HashMap<Object,Object> map = new HashMap<>();
+			JSONObject object = (JSONObject) iterator.next();
+			if(null != object.get("createdTaskIDs")) {
+				JSONArray taskIdsArray = object.getJSONArray("createdTaskIDs");
+				for (int i = 0; i < taskIdsArray.size(); i++) {
+					if (String.valueOf(taskId).equals(taskIdsArray.get(i))) {
+						map.put("tokenId", object.get("tokenId"));
+						for (int j = flag; j >= 0; j--) {
+							if(preTokenIdList.get(j) != null) {
+								map.put("preTokenId", preTokenIdList.get(j));
+							}else {
+								map.put("preTokenId", null);
 							}
 						}
-						return hashMap;
+						return map;
 					}
+				}	
+			}else{
+				JSONArray jsonArray2 = object.getJSONArray("children");
+				preTokenIdList.add(object.get("tokenId"));
+				flag++;
+				HashMap util = util(jsonArray2, taskId);
+				if(util != null) {
+					return util;
 				}
 			}
-			flag++;
-		}
-		JSONArray jsonArray2 = result.getJSONArray("children");
-		if (null != jsonArray2) {
-			hashMap = util(jsonArray2, taskId);
-		}
-		if (hashMap != null) {
-			return hashMap;
 		}
 		return null;
 	}
+	
 }
