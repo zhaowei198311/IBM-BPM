cf3f4b113571a9b43c1019049aaf7843c2041ceb
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-13 02:07:25 +0000

    修改rabbitmq消费者 确认为手动

--- bpm/src/main/java/com/desmart/desmartbpm/mq/rabbit/MQConsumer.java
+++ bpm/src/main/java/com/desmart/desmartbpm/mq/rabbit/MQConsumer.java
@@ -6,22 +6,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.rabbitmq.client.Channel;
 
 /**
  * rabbitMQ消费者，需要实现MessageListener（不同业务场景的消费者和监听器绑定的过程是在配置文件中实现的）
  * @author zbw
  *
  */
-public class MQConsumer implements MessageListener{
+public class MQConsumer implements ChannelAwareMessageListener{
 	
-	private static final Logger log = LoggerFactory.getLogger(MQConsumer.class);
-
+private static final Logger log = LoggerFactory.getLogger(MQConsumer.class);
+	
 	@Override
-	public void onMessage(Message message){
+	public void onMessage(Message message, Channel channel) throws Exception {
 		
 		System.out.println("rabbitmq接受到消息：" + message);
 		String str = null;
@@ -31,9 +33,12 @@ public class MQConsumer implements MessageListener{
 		} catch (UnsupportedEncodingException e) {
 			log.info("获取message中body内容出错...");
 			e.printStackTrace();
+		}finally {
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 		}
-		JSONObject json = JSONObject.parseObject(message.toString());
-		System.out.println("=====" + json.get("accountType"));
+		/*JSONObject json = JSONObject.parseObject(message.toString());
+		System.out.println("=====" + json.get("accountType"));*/
 	}
+
 
 }
