fa3d676c908ddd5ef3be1db6d4a71b136e6bdb7f
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-14 05:51:46 +0000

    修改数据提取工具类

--- bpm/src/main/java/com/desmart/common/util/ExecutionTreeUtil.java
+++ bpm/src/main/java/com/desmart/common/util/ExecutionTreeUtil.java
@@ -14,39 +14,41 @@ import com.alibaba.fastjson.JSONObject;
  *
  */
 public class ExecutionTreeUtil {
-	
+   
 	static int flag = 0;
-	
-    /**
-     * 从流程详细信息中获得指定任务的tokenId和父tokenId
-     * @param taskId 任务ID
-     * @param json 由流程详细信息转化来的JSONObject
-     * @return
-	 * Map中的key ： 1）tokenId 2) preTokenId
-     */
-	public static final Map<Object, Object> queryTokenId(int taskId, JSONObject json){
-    	// 解析jsonObject
-    	JSONObject jsonObject = json.getJSONObject("data").getJSONObject("executionTree").getJSONObject("root");
+
+	/**
+	 * 从流程详细信息中获得指定任务的tokenId和父tokenId
+	 * @param taskId ： 任务ID
+	 * @param json ：由流程详细信息转化来的JSONObject
+	 * @return Map中的key ： 1）tokenId 2) preTokenId
+	 */
+	public static final Map<Object, Object> queryTokenId(int taskId, JSONObject json) {
+		// 解析jsonObject
+		JSONObject jsonObject = json.getJSONObject("data").getJSONObject("executionTree").getJSONObject("root");
 		JSONArray jsonArray = jsonObject.getJSONArray("children");
-		ArrayList<Object> preTokenIdList = new ArrayList<>();
-		//第一次遍历children数组对象
-		HashMap<Object,Object> hashMap = new HashMap<>();
+		ArrayList<HashMap<String, Object>> preTokenIdList = new ArrayList<>();
+		// 第一次遍历children数组对象
+		HashMap<Object, Object> hashMap = new HashMap<>();
 		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
 			JSONObject object = (JSONObject) iterator.next();
-			if(null != object.get("createdTaskIDs")) {
-				//此处比较createdTaskIDs数组中是否包含taskId
+			if (null != object.get("createdTaskIDs")) {
+				// 此处比较createdTaskIDs数组中是否包含taskId
 				JSONArray taskIdsArray = object.getJSONArray("createdTaskIDs");
 				for (int i = 0; i < taskIdsArray.size(); i++) {
 					if (String.valueOf(taskId).equals(taskIdsArray.get(i))) {
 						hashMap.put("tokenId", object.get("tokenId"));
-						//第一次循环时，preTokenId为null
-				        hashMap.put("preTokenId", null);
-				        return hashMap;
+						// 第一次循环时，preTokenId为null
+						hashMap.put("preTokenId", null);
+						return hashMap;
 					}
 				}
-			}else{
-				//每次循环下级时候把此级tokenId放入list中
-				preTokenIdList.add(object.get("tokenId"));
+			} else {
+				// 每次循环下级时候把此级tokenId放入list中
+				HashMap<String, Object> map = new HashMap<>();
+				map.put("tokenId", object.get("tokenId"));
+				map.put("flowObjectId", object.get("flowObjectId"));
+				preTokenIdList.add(map);
 				JSONArray jsonArray2 = object.getJSONArray("children");
 				HashMap utilMap = util(jsonArray2, taskId, preTokenIdList);
 				if (utilMap != null) {
@@ -57,96 +59,108 @@ public class ExecutionTreeUtil {
 			}
 		}
 		return null;
-    }
-	
-	public static HashMap<Object, Object> util(JSONArray jsonArray, int taskId, ArrayList preTokenIdList) {
-		
-		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
-			HashMap<Object,Object> map = new HashMap<>();
+	}
+
+	public static HashMap<Object, Object> util(JSONArray jsonArray, int taskId,
+			ArrayList<HashMap<String, Object>> preTokenIdList) {
+
+		for (Iterator<?> iterator = jsonArray.iterator(); iterator.hasNext();) {
+			HashMap<Object, Object> map = new HashMap<>();
 			JSONObject object = (JSONObject) iterator.next();
-			if(null != object.get("createdTaskIDs")) {
+			if (null != object.get("createdTaskIDs")) {
 				JSONArray taskIdsArray = object.getJSONArray("createdTaskIDs");
 				for (int i = 0; i < taskIdsArray.size(); i++) {
 					if (String.valueOf(taskId).equals(taskIdsArray.get(i))) {
 						map.put("tokenId", object.get("tokenId"));
-						//此处赋值上级preTokenId
+						// 此处赋值上级preTokenId
+						String flowObjectId = (String) object.get("flowObjectId");
 						for (int j = flag; j >= 0; j--) {
-							if(preTokenIdList.get(j) != null) {
-								map.put("preTokenId", preTokenIdList.get(j));
-								return map;
-							}else {
+							if (preTokenIdList.get(j) != null) {
+								if (preTokenIdList.get(j).get("flowObjectId") != null
+										&& !preTokenIdList.get(j).get("flowObjectId").equals(flowObjectId)) {
+									map.put("preTokenId", preTokenIdList.get(j).get("tokenId"));
+									return map;
+								}
+							} else {
 								map.put("preTokenId", null);
 							}
 						}
 						return map;
 					}
-				}	
-			}else{
-				JSONArray jsonArray2 = object.getJSONArray("children");
-				preTokenIdList.add(object.get("tokenId"));
-				flag++;
-				HashMap util = util(jsonArray2, taskId, preTokenIdList);
-				if(util != null) {
+				}
+			}
+			HashMap<String, Object> currentNodeMap = new HashMap<>();
+			currentNodeMap.put("tokenId", object.get("tokenId"));
+			currentNodeMap.put("flowObjectId", object.get("flowObjectId"));
+			preTokenIdList.add(currentNodeMap);
+			JSONArray jsonArray2 = object.getJSONArray("children");
+			//每深入一级，flag加1s
+			flag++;
+			if (null != jsonArray2) {
+				HashMap<Object, Object> util = util(jsonArray2, taskId, preTokenIdList);
+				if (util != null) {
 					return util;
 				}
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 获取preFlowObjectId同级下的tokenId
-	 * @param json JSONObject对象
-	 * @param preFlowObjectId  父节点tokenId
-	 * @param childFlowObjectId  子节点tokenId
+	 * @param json： JSONObject对象
+	 * @param preFlowObjectId：父节点tokenId
+	 * @param childFlowObjectId：子节点tokenId
 	 * @return preFlowObjectId节点下的tokenId
 	 */
 	public static String queryCurrentNodeTokenId(JSONObject json, String preFlowObjectId, String childFlowObjectId) {
-		
+
 		JSONObject jsonObject = json.getJSONObject("data").getJSONObject("executionTree").getJSONObject("root");
 		JSONArray jsonArray = jsonObject.getJSONArray("children");
-		//第一次遍历children数组对象
+		// 第一次遍历children数组对象
 		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) {
 			JSONObject object = (JSONObject) iterator.next();
-			if(preFlowObjectId.equals(object.get("flowObjectId"))) {
+			if (preFlowObjectId.equals(object.get("flowObjectId"))) {
 				JSONArray childArray = object.getJSONArray("children");
-				if(null != childArray) {
+				if (null != childArray) {
 					boolean resultFlag = queryCurrentTokenIdUtil(childArray, preFlowObjectId, childFlowObjectId);
-					if(resultFlag) {
+					if (resultFlag) {
 						String resultStr = object.get("tokenId").toString();
-						return  resultStr;
-						//System.out.println("***" + object.get("tokenId"));
+						return resultStr;
+						// System.out.println("***" + object.get("tokenId"));
 					}
 				}
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 获取preFlowObjectId同级下的tokenId辅助方法
+	 * 
 	 * @param jsonArray
 	 * @param preFlowObjectId
 	 * @param childFlowObjectId
 	 * @return
 	 */
-    public static boolean queryCurrentTokenIdUtil(JSONArray jsonArray, String preFlowObjectId, String childFlowObjectId) {
-		
+	public static boolean queryCurrentTokenIdUtil(JSONArray jsonArray, String preFlowObjectId,
+			String childFlowObjectId) {
+
 		for (int i = 0; i < jsonArray.size(); i++) {
 			JSONObject object = (JSONObject) jsonArray.get(i);
 			Object flowObjectId = object.get("flowObjectId");
-			if(null == flowObjectId) {
+			if (null == flowObjectId) {
 				JSONArray childArray = object.getJSONArray("children");
-				if(null == childArray) {
+				if (null == childArray) {
 					return false;
-				}else {
-					boolean flag = queryCurrentTokenIdUtil(childArray, preFlowObjectId,  childFlowObjectId);
-					if(flag) {
+				} else {
+					boolean flag = queryCurrentTokenIdUtil(childArray, preFlowObjectId, childFlowObjectId);
+					if (flag) {
 						return true;
 					}
 				}
-			}else {
-				if(childFlowObjectId.equals(flowObjectId)) {
+			} else {
+				if (childFlowObjectId.equals(flowObjectId)) {
 					return true;
 				}
 			}
