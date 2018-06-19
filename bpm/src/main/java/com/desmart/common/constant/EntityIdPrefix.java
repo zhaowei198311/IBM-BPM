79bb928f09cb76c8e9791a6f373e1c74370c398a
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-06-19 08:29:57 +0000

    新增调用触发器异常实体类，此文件新增定义id生成规则信息

--- bpm/src/main/java/com/desmart/common/constant/EntityIdPrefix.java
+++ bpm/src/main/java/com/desmart/common/constant/EntityIdPrefix.java
@@ -79,4 +79,6 @@ public class EntityIdPrefix {
 	public static final String DH_INSTANCE_FILE_CARD = "file_card:";
     /** 简单循环任务的任务处理人 */
 	public static final String DH_TASK_HANDLER = "task_handler:";
+	/** triggerMQ消费者执行出错异常类 */
+	public static final String DH_TRIGGER_EXCEPTION = "trigger_exception";
 }
