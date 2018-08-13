142b92039ff9b67e3219a5f33d89e7d229ba3602
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-08-13 01:57:16 +0000

    门户集成需要的新开店信息的serviceImpl

---
+++ bpm/src/main/java/com/desmart/desmartbpm/service/impl/DhNewStoreInfoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.desmart.desmartbpm.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.desmart.desmartbpm.service.DhNewStoreInfoService;
+
+@Service
+public class DhNewStoreInfoServiceImpl implements DhNewStoreInfoService{
+	
+
+}
