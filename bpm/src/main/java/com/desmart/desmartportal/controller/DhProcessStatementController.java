39a313da6620a930d8c795455310d93db4bcdccb
xushangfei <xushangfei@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-08-13 01:49:24 +0000

    报表Controller

---
+++ bpm/src/main/java/com/desmart/desmartportal/controller/DhProcessStatementController.java
@@ -0,0 +1,91 @@
+package com.desmart.desmartportal.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.poi.util.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.desmart.common.constant.ServerResponse;
+import com.desmart.desmartportal.entity.DhTaskInstance;
+import com.desmart.desmartportal.service.DhProcessStatementService;
+import com.desmart.desmartportal.util.ExcelReport;
+import com.desmart.desmartsystem.util.MyDateUtils;
+
+/**
+ * <p>
+ * Title: 流程报表
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * @author xsf
+ * @date 2018年8月7日
+ */
+@Controller
+@RequestMapping("/projectStatement")
+public class DhProcessStatementController {
+
+	@Autowired
+	private DhProcessStatementService dhProcessStatementService;
+
+	/**
+	 * 流程报表1页面跳转
+	 */
+	@RequestMapping(value = "/projectStatement")
+	public String projectStatement() {
+		return "desmartportal/statement/projectStatement";
+	}
+
+	/**
+	 * 
+	 * 根据条件查询流程带分页
+	 * 
+	 * @param Map<String,
+	 *            String> parameter
+	 * @param pageNum
+	 *            第几页
+	 * @param pageSize
+	 *            每页最大显示10
+	 * @return
+	 */
+	@RequestMapping(value = "/queryTaskInstance")
+	@ResponseBody
+	private ServerResponse queryTaskInstance(@RequestParam Map<String, String> parameter, Integer pageNum,
+			Integer pageSize) {
+		System.out.println(pageNum);
+		System.out.println(pageSize);
+
+		return dhProcessStatementService.selectAllTask(parameter, pageNum, pageSize);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "/queryExportTaskInstance")
+	private void queryExportTaskInstance(@RequestParam Map<String, String> parameter,HttpServletResponse response) throws Exception {
+		List<DhTaskInstance> dhTaskInstanceList  = dhProcessStatementService.selectAllTask(parameter);
+		Map bean = new HashMap();
+		bean.put("createDate",MyDateUtils.getCurrentDate());
+		bean.put("dhTaskInstanceList", dhTaskInstanceList);
+		ExcelReport   excelReport=new ExcelReport();
+		InputStream excInputStream = excelReport.makeReportFromTemplet("/Users/xsf/eclipse-workspace/bpm/src/main/webapp/resources/excel-template/流程过程业务数据.xls", bean);
+		OutputStream os = response.getOutputStream();
+        response.addHeader("Content-Disposition",
+				"attachment;filename=" + new String(("流程过程业务数据" + ".xls").getBytes("utf-8"), "ISO-8859-1"));
+		IOUtils.copy(excInputStream, os);
+	}
+
+}
