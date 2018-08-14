7ac3d7113c5e44b361e508acfd7fd067c2ac95bb
zhangbowen <zhangbowen@6b90bfc9-d56e-b242-9026-d3b23ea1a663>
2018-08-14 07:29:57 +0000

    根据日期自增id工具类文件更新

--- bpm/src/main/java/com/desmart/common/util/IncrementIdByDateUtil.java
+++ bpm/src/main/java/com/desmart/common/util/IncrementIdByDateUtil.java
@@ -5,22 +5,16 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.locks.ReentrantLock;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.alibaba.fastjson.JSONObject;
 import com.desmart.desmartsystem.entity.BpmGlobalConfig;
 import com.desmart.desmartsystem.service.BpmGlobalConfigService;
-import com.desmart.desmartsystem.service.impl.BpmGlobalConfigServiceImpl;
 
 /**
- * 此方法可根据当前日期生成当天自增长的id值，格式为 20180806-000111，第二天的数据从20180807-000001开始
+ * 按日期自增id类
  * @author zbw
  *
  */
@@ -28,16 +22,19 @@ import com.desmart.desmartsystem.service.impl.BpmGlobalConfigServiceImpl;
 public class IncrementIdByDateUtil {
 
 	private static final Logger log = LoggerFactory.getLogger(IncrementIdByDateUtil.class);
+	
 	@Autowired
 	private BpmGlobalConfigService bpmGlobalConfigService;
-	//加锁
+	
 	private static ReentrantLock lock = new ReentrantLock();
 	
 	//private ExecutorService threadPool = Executors.newFixedThreadPool(5);
-	
+	/**
+	 * 此方法可根据当前日期生成当天自增长的id值，格式为 20180806-000111，第二天的数据从20180807-000001开始
+	 * @return
+	 */
 	public String createId() {
 		String result = null;
-		//拿到第一个bpmGlobalConfig对象并判断
 		lock.lock();
 		try {
 			BpmGlobalConfig bpmGlobalConfig = bpmGlobalConfigService.getFirstActConfig();
@@ -66,7 +63,7 @@ public class IncrementIdByDateUtil {
 				log.info("返回结果{}", result);
 				return result;
 				//idValue = (int) parseObject.get("id");
-			}else {
+			}else {   //当日期不是今天的时候，重新从1开始生成
 				JSONObject json = new JSONObject();
 				json.put("date", toDay);
 				json.put("id", 1);
